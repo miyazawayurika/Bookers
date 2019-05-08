158025db18292c30c2adad06ebd3297941920036
miyazawayurika <miyazawayurika@mba.local>
2019-05-08 20:29:15 +0900

    [Finish]2-8

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'root/top'
+  resources :posts
+  root 'root#top'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'top' => 'root#top'
+  get 'index' => 'posts#index'
+  get  'posts/new' => 'posts#new'
+  post 'posts' => 'posts#create'
+
+end
